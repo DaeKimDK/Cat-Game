4087b3a0466df6a7e44f544d326c8ae9c07a44df
jellesse92 <jehunter@uci.edu>
2016-01-16 08:23:13 -0800

    Creating Base Cat Prefabs

---
+++ Cat Game/Assets/Scripts/Cat_Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cat_Movement : MonoBehaviour {
+
+    public float speed = 1.0f;
+    string direction;
+    // Use this for initialization
+    void Start () {
+        direction = "left";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int movedir = direction == "left" ? 1 : -1;
+        var move = new Vector3(movedir*1,0,0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
