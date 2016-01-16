472ab1835bcd0652b746f46c7db4550655a856d9
Tom Tan <tomtan117@gmail.com>
2016-01-16 13:08:33 -0800

    Added GameState Script
    Display Total cats
    Display Visible Cats (not compelete)
    Countdown Timer

--- Cat Game/Assets/Scripts/GameState.cs
+++ Cat Game/Assets/Scripts/GameState.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class GameState : MonoBehaviour {
     int num_of_cats;
+    float timeLeft = 30;
 	// Use this for initialization
 	void Start () {
         num_of_cats = 0;
@@ -11,6 +12,11 @@ public class GameState : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         num_of_cats = GameObject.FindGameObjectsWithTag("Cat").Length;
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            timeLeft = 30;
+        }
 	}
 
     void OnGUI()
@@ -18,6 +24,8 @@ public class GameState : MonoBehaviour {
         GUIStyle style = new GUIStyle(GUI.skin.GetStyle("label"));
         style.fontSize = 30;
         GUI.color = Color.black;
-        GUI.Label(new Rect(30,30, Screen.width / 2, Screen.height / 2), num_of_cats.ToString(), style);
+        GUI.Label(new Rect(25,25, Screen.width / 2, Screen.height / 2), "Total: " + num_of_cats, style);
+        GUI.Label(new Rect(25, 60, Screen.width / 2, Screen.height / 2), "Visible: " + num_of_cats, style);
+        GUI.Label(new Rect(1200, 25, Screen.width / 2, Screen.height / 2), "Time: " + timeLeft, style);
     }
 }
