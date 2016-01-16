fc7e8b37b3031d74d357ae5ba1cca43a326562fc
jellesse92 <jehunter@uci.edu>
2016-01-16 12:19:00 -0800

    Adjusting Cat Prefab and adding Sample Art

--- Cat Game/Assets/Scripts/Cat_Movement.cs
+++ Cat Game/Assets/Scripts/Cat_Movement.cs
@@ -4,16 +4,37 @@ using System.Collections;
 public class Cat_Movement : MonoBehaviour {
 
     public float speed = 1.0f;
+    public bool hidden;
+    public float hide_time = 10.0F;
     string direction;
+    float walkTime;
+    Vector3 location = new Vector3(1, 0, 0);
+
     // Use this for initialization
     void Start () {
-        direction = "left";
-	}
+        walkTime = Random.Range(5.0F, 10.0F) ; //  How long will the cat walk in this direction;
+        hidden = true;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        int movedir = direction == "left" ? 1 : -1;
-        var move = new Vector3(movedir*1,0,0);
-        transform.position += move * speed * Time.deltaTime;
+        walkTime -= Time.deltaTime;
+        if (hidden == true)
+        {
+            hide_time -= Time.deltaTime;
+            GetComponent.enabled = false;
+            return;
+        }
+        if(walkTime <= 0){ location = flipDirection(location); }
+        transform.position += location * speed * Time.deltaTime;
+    }
+    
+    Vector3 flipDirection(Vector3 originalDir)
+    {
+        walkTime = Random.Range(5.0F, 10.0F);
+        transform.Rotate(0, 180, 0, Space.Self);
+        return new Vector3(-1 * originalDir.x, 0, 0); 
     }
 }
+
