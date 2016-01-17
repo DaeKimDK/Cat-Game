cf6f4ff0af0f7c0c56dd3e00001989cb5d4c228b
Michelle Wong <michellewong.nma@gmail.com>
2016-01-16 18:52:53 -0800

    cat hides on collision with furniture

--- Cat Game/Assets/Scripts/Furniture.cs
+++ Cat Game/Assets/Scripts/Furniture.cs
@@ -1,34 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.EventSystems;
 
-public class Furniture : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler{
-	//public int CAPACITY = 3; // how many cats this furniture can contain.
-	//GameObject[] slots;
+public class Furniture : MonoBehaviour{ //IDropHandler, IPointerEnterHandler, IPointerExitHandler{
+	public GameObject manager;
 
-	//these two functions are needed in order for OnDrop to work
-	public void OnPointerEnter(PointerEventData eventData){
-	}
-	public void OnPointerExit(PointerEventData eventData){
-	}
-
-	void Start(){
-		//slots = new GameObject[CAPACITY];
-	}
-	/*
-	public void OnTriggerEnter(Collider cat){
+	void OnTriggerEnter2D(Collider2D cat){
 		Debug.Log ("OnTriggerEnter");
-		Color color = cat.gameObject.GetComponent<Renderer>().material.color;
-		color.a -= 1.0f;
+		//Color color = cat.gameObject.GetComponent<Renderer>().material.color;
+		//color.a -= 1.0f;
 		//cat.gameObject.GetComponent<Renderer>().material.color.a = 0.0f;
-		cat.gameObject.SetActive (false);
-	}*/
+		//if(cat.gameObject.tag == "Cat"){
 
+		cat.gameObject.SetActive (false);
+	}
+		
+	/*
 	public void OnDrop( PointerEventData eventData){
 		Debug.Log ("OnDrop");
 		Draggable cat = eventData.pointerDrag.GetComponent<Draggable> ();
 		cat.gameObject.SetActive (false);
 
-	}
+	}*/
 
 }
